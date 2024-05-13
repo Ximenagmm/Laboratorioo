c69596fea3bcd26475857d3cea53e73e00adcbec
Ximenagmm <Xgiron@correo.url.edu.gt>
2024-05-13 10:50:03 -0600

    Create Ejercicio 3 xbgm.py

---
+++ Ejercicio 3 xbgm.py
@@ -0,0 +1,30 @@
+numero= int ( input("Ingrese numero: "))
+I= 1 
+IV= 4
+V= 5 
+IV= 9 
+X= 10 
+XL=40 
+L= 50 
+LX= 60 
+LXX= 70
+LXXX= 80
+XC= 90 
+C = 100
+CC= 200
+CCC= 300
+CD= 400
+D= 500
+DC= 600
+DCC=700
+DCCC=800
+CM= 900
+M =1000
+
+if numero <1 or numero > 999:
+    print=("Numero no valido")
+
+else: 
+    if (numero == 999):
+        print("CMXCLX")
+
